7e9f23bc37eb7fa21af8742662e25582324d242b
Dan Suh <kaehops@gmail.com>
2018-07-20 00:45:14 +0900

    Cleaner code + gpu utilization

--- model.py
+++ model.py
@@ -1,3 +1,9 @@
+"""
+Implementation of AlexNet, from paper
+"ImageNet Classification with Deep Convolutional Neural Networks" by Alex Krizhevsky et al.
+
+See: https://papers.nips.cc/paper/4824-imagenet-classification-with-deep-convolutional-neural-networks.pdf
+"""
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -6,79 +12,122 @@ from torch.utils import data
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 
+# define pytorch device - useful for device-agnostic execution
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# define model parameters
+NUM_EPOCHS = 90
+BATCH_SIZE = 128
+MOMENTUM = 0.9
+LR_DECAY = 0.0005
+LR_INIT = 0.01
+IMAGE_DIM = 227  # pixels
+NUM_CLASSES = 200  # 200 classes for tiny imagenet
+TRAIN_IMG_DIR = 'alexnet_data_in/tiny-imagenet-200/train'
+DEVICE_IDS = [0, 1]
+
 
 class AlexNet(nn.Module):
+    """
+    Neural network model consisting of layers propsed by AlexNet paper.
+    """
     def __init__(self, num_classes=1000):
+        """
+        Define and allocate layers for this neural net.
+
+        Args:
+            num_classes (int): number of classes to predict with this model
+        """
         super().__init__()
         # input size should be : (b x 3 x 227 x 227)
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=4)  # (b x 96 x 55 x 55)
-        self.relu1 = nn.ReLU()
-        self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2)  # (b x 96 x 27 x 27)
-        self.conv2 = nn.Conv2d(96, 256, 5, padding=2)  # (b x 256 x 27 x 27)
-        self.relu2 = nn.ReLU()
-        self.maxpool2 = nn.MaxPool2d(kernel_size=3, stride=2)  # (b x 256 x 13 x 13)
-        self.conv3 = nn.Conv2d(256, 384, 3, padding=1)  # (b x 384 x 13 x 13)
-        self.relu3 = nn.ReLU()
-        self.conv4 = nn.Conv2d(384, 384, 3, padding=1)  # (b x 384 x 13 x 13)
-        self.relu4 = nn.ReLU()
-        self.conv5 = nn.Conv2d(384, 256, 3, padding=1)  # (b x 256 x 13 x 13)
-        self.relu5 = nn.ReLU()
-        self.maxpool3 = nn.MaxPool2d(kernel_size=3, stride=2)  # (b x 256 x 6 x 6)
-        self.fc1 = nn.Linear(in_features=(256 * 6 * 6), out_features=4096)
-        self.relu6 = nn.ReLU()
-        self.fc2 = nn.Linear(in_features=4096, out_features=4096)
-        self.relu7 = nn.ReLU()
-        # dropout 0.5?
-        self.fc3 = nn.Linear(in_features=4096, out_features=num_classes)
+        # The image in the original paper states that width and height are 224 pixels, but
+        # the dimensions after first convolution layer do not lead to 55 x 55.
+        self.net = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=4),  # (b x 96 x 55 x 55)
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),  # (b x 96 x 27 x 27)
+            nn.Conv2d(96, 256, 5, padding=2),  # (b x 256 x 27 x 27)
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),  # (b x 256 x 13 x 13)
+            nn.Conv2d(256, 384, 3, padding=1),  # (b x 384 x 13 x 13)
+            nn.ReLU(),
+            nn.Conv2d(384, 384, 3, padding=1),  # (b x 384 x 13 x 13)
+            nn.ReLU(),
+            nn.Conv2d(384, 256, 3, padding=1),  # (b x 256 x 13 x 13)
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),  # (b x 256 x 6 x 6)
+        )
+        # classifier is just a name for linear layers
+        self.classifier = nn.Sequential(
+            nn.Linear(in_features=(256 * 6 * 6), out_features=4096),
+            nn.ReLU(),
+            nn.Dropout2d(p=0.5, inplace=True),
+            nn.Linear(in_features=4096, out_features=4096),
+            nn.ReLU(),
+            nn.Dropout2d(p=0.5, inplace=True),
+            nn.Linear(in_features=4096, out_features=num_classes),
+        )
 
     def forward(self, x):
-        x = self.relu1(self.conv1(x))
-        x = self.maxpool1(x)
-        x = self.relu2(self.conv2(x))
-        x = self.maxpool2(x)
-        x = self.relu3(self.conv3(x))
-        x = self.relu4(self.conv4(x))
-        x = self.relu5(self.conv5(x))
-        x = self.maxpool3(x)
-        # change shape for linear feed
-        x = x.view(x.size(0), 256 * 6 * 6)
-        x = self.relu6(self.fc1(x))
-        x = self.relu7(self.fc2(x))
-        return self.fc3(x)
+        """
+        Pass the input through the net.
+
+        Args:
+            x (Tensor): input tensor
+
+        Returns:
+            output (Tensor): output tensor
+        """
+        x = self.net(x)
+        x = x.view(-1, 256 * 6 * 6)  # reduce the dimensions for linear layer input
+        return self.classifier(x)
 
 
-alexnet = AlexNet()
-a = torch.randn((1, 3, 227, 227))
-print(alexnet)
-print(a.size())
-out = alexnet(a)
-print(out.size())
+if __name__ == '__main__':
+    # create model
+    alexnet = AlexNet(num_classes=200).to(device)
+    # train on multiple GPUs
+    alexnet = torch.nn.parallel.DataParallel(alexnet, device_ids=DEVICE_IDS)
+    print(alexnet)
+    print('AlexNet created')
 
+    # create dataset and data loader
+    imagenet_dataset = datasets.ImageFolder(TRAIN_IMG_DIR, transforms.Compose([
+        transforms.RandomResizedCrop(IMAGE_DIM),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]))
+    print('Dataset created')
+    imagenet_dataloader = data.DataLoader(
+        imagenet_dataset,
+        shuffle=True,
+        batch_size=64)
+    print('Dataloader created')
 
-traindir = 'alexnet_data_in/tiny-imagenet-200/train'
-imagenet_dataset = datasets.ImageFolder(traindir, transforms.Compose([
-    transforms.RandomResizedCrop(227),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-]))
-print('Dataset created')
+    # create optimizer
+    optimizer = optim.SGD(
+        params=alexnet.parameters(),
+        lr=LR_INIT,
+        momentum=MOMENTUM,
+        weight_decay=LR_DECAY)
+    print('Optimizer created')
 
-imagenet_dataloader = data.DataLoader(
-    imagenet_dataset,
-    shuffle=True,
-    batch_size=64)
-print('Dataloader created')
+    # start training!!
+    print('Starting training...')
+    total_steps = 1
+    for epoch in range(NUM_EPOCHS):
+        for imgs, classes in imagenet_dataloader:
+            imgs, classes = imgs.to(device), classes.to(device)
+            optimizer.zero_grad()
 
-optimizer = optim.Adam(alexnet.parameters(), lr=0.001)
-print('Optimizer created')
+            # calculate the loss
+            output_logits = alexnet(imgs)
+            loss = F.nll_loss(F.log_softmax(output_logits), target=classes)
+            print('Epoch: {} \tStep: {} \tLoss: {}'.format(epoch + 1, total_steps, loss.item()))
 
-print('Starting training...')
-for imgs, classes in imagenet_dataloader:
-    print(classes)
-    output_logits = alexnet(imgs)
-    optimizer.zero_grad()
-    loss = F.nll_loss(F.log_softmax(output_logits), target=classes)
-    print('Loss: {}'.format(loss.item()))
-    loss.backward()
-    optimizer.step()
+            # update the parameters
+            loss.backward()
+            optimizer.step()
+
+            total_steps += 1
